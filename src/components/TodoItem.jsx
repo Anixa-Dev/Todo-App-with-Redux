1aebe3b5cbb90ee029f222f3348a194c822e2ad8
Anixa <anixabhayani@gmail.com>
2025-04-28 13:47:53 +0530

    save changes

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -16,19 +16,19 @@ function TodoItem({ todo, index }) {
             </div>
 
             <div className='space-x-3 ml-8'>
-                <button onClick={() => dispatch(toggleTodo(index))} className='mr-2 text-sm bg-blue-500 text-white sm:py-2 py-1 px-1 rounded'>{todo.completed ? <FaToggleOff /> : <FaToggleOn />}</button>
+                <button onClick={() => dispatch(toggleTodo(index))} className='mr-2 text-sm bg-blue-500 text-white  py-1 px-2 rounded'>{todo.completed ? <FaToggleOff /> : <FaToggleOn />}</button>
 
-                <button onClick={() => dispatch(removeTodo(index))} className='mr-2 text-sm bg-red-500 text-white sm:py-2 py-1 px-1 rounded'><FaTrash /></button>
+                <button onClick={() => dispatch(removeTodo(index))} className='mr-2 text-sm bg-red-500 text-white  py-1 px-2 rounded'><FaTrash /></button>
 
                 {
                     !todo.completed && (
-                        <button onClick={() => dispatch(markCompleted(index))} className='mr-2 text-sm bg-blue-500 text-white  py-1 px-1 rounded'><FaCheck /></button>
+                        <button onClick={() => dispatch(markCompleted(index))} className='mr-2 text-sm bg-blue-500 text-white  py-1 px-2 rounded'><FaCheck /></button>
                     )
                 }
 
                 {
                     todo.completed && (
-                        <button onClick={() => dispatch(markIncomplete(index))} className='mr-2 text-sm bg-yellow-500 text-white sm:py-2 py-1 px-1 rounded'><FaTimes /></button>
+                        <button onClick={() => dispatch(markIncomplete(index))} className='mr-2 text-sm bg-yellow-500 text-white  py-1 px-2 rounded'><FaTimes /></button>
                     )
                 }
 
